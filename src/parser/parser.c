c8b4afded073131da90d4cf11a70c5de20f7e5fe
ta-zewen <zevin@mail.ustc.edu.cn>
2023-09-14 17:11:31 +0000

    fixed a bug related to parser.c; removed unused dir

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 
     if (argc == 2) {
         input = argv[1];
-    } else {
+    } else if(argc >= 3) {
         printf("usage: %s <cminus_file>\n", argv[0]);
         return 1;
     }
